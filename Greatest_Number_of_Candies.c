159212b1e66f993e7b579607e190ce1f129db6c9
22P31A0548 <119731878+22P31A0548@users.noreply.github.com>
2022-12-31 19:56:09 +0530

    #include<stdio.h> int main() { 	int n,i,max=0; 	scanf("%d",&n); 	int arr[n]; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]); 	} 	for(i=0;i<n;i++) 	{ 		if(arr[i]>max) 		{ 			max=arr[i]; 		} 	} 	int m; 	scanf("%d",&m); 	for(i=0;i<n;i++) 	{ 		if(arr[i]+m>=max) 	{ 		printf("True "); 	} 	else 	{ 		printf("False "); 	} 	}
    }

---
+++ Greatest_Number_of_Candies.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int n,i,max=0;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]>max)
+		{
+			max=arr[i];
+		}
+	}
+	int m;
+	scanf("%d",&m);
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]+m>=max)
+	{
+		printf("True ");
+	}
+	else
+	{
+		printf("False ");
+	}
+	}
+	
+}
