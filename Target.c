07b17fcdc70bb5e9e675b4001edb41826fefaa26
22P31A0548 <119731878+22P31A0548@users.noreply.github.com>
2023-01-03 19:16:55 +0530

    #include<stdio.h> int main() {     int  P1p1,P2p2,P3p3,P4p4;     scanf("%d%d%d%d",&P1p1,&P2p2,&P3p3,&P4p4);     if(P1p1>=10&&P2p2>=10&&P3p3>=10&&P4p4>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int  P1p1,P2p2,P3p3,P4p4;
+    scanf("%d%d%d%d",&P1p1,&P2p2,&P3p3,&P4p4);
+    if(P1p1>=10&&P2p2>=10&&P3p3>=10&&P4p4>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
