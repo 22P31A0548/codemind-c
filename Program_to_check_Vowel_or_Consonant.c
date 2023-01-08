7d2b5230ce574fb8c2dc6c3094823d8b89306af3
22P31A0548 <119731878+22P31A0548@users.noreply.github.com>
2023-01-08 14:30:19 +0530

    #include<stdio.h> int main() {     char ch;     scanf("%c",&ch);     if(ch=='a')     {         printf("Vowel");     }      else if(ch=='e')     {         printf("Vowel");     }      else if(ch=='i')     {         printf("Vowel");     }      else if(ch=='o')     {         printf("Vowel");     }      else if(ch=='u')     {         printf("Vowel");     }      else if(ch=='A')     {         printf("Vowel");     }      else if(ch=='E')     {         printf("Vowel");     }      else if(ch=='I')     {         printf("Vowel");     }      else if(ch=='O')     {         printf("Vowel");     }      else if(ch=='U')     {         printf("Vowel");     }     else     {         printf("Consonant");     } }

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='a')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='e')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='i')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='o')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='u')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='A')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='E')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='I')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='O')
+    {
+        printf("Vowel");
+    }
+     else if(ch=='U')
+    {
+        printf("Vowel");
+    }
+    else
+    {
+        printf("Consonant");
+    }
+}
