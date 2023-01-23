b969f16b74f504ec146025e1c0c741b994f1f534
22P31A0548 <119731878+22P31A0548@users.noreply.github.com>
2023-01-23 11:08:40 +0530

    #include<stdio.h> int main() {    int n,i,a,b;    scanf("%d %d %d",&n,&a,&b);    for(i=a;i<=b;i++)    {        printf("%d x %d = %d ",n,i,n*i);    } }

--- Multiplication_Table_3.c
+++ Multiplication_Table_3.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,a,b,i;
-    scanf("%d%d%d",&n,&a,&b);
-    for(i=a;i<=b;i++)
-    {
-        printf("%d x %d = %d
+   int n,i,a,b;
+   scanf("%d %d %d",&n,&a,&b);
+   for(i=a;i<=b;i++)
+   {
+       printf("%d x %d = %d
 ",n,i,n*i);
-    }
-}
+   }
+}  
