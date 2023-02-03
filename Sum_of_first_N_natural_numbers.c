0aac41f02a2a283bd0de52d1ffc2fa51651a4a90
22P31A0548 <119731878+22P31A0548@users.noreply.github.com>
2023-02-03 21:35:26 +0530

    #include<stdio.h> int main() {     int i,n,sum=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         sum=sum+i;     }     printf("%d",sum); }

--- Sum_of_first_N_natural_numbers.c
+++ Sum_of_first_N_natural_numbers.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 int main()
 {
-   int n,i,sum=0;
-   scanf("%d",&n);
-   for(i=1;i<=n;i++)
-  {
-      sum=sum+i;
-  }
-  printf("%d ",sum);
-}  
-      
+    int i,n,sum=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+}
